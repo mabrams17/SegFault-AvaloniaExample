f1e79244cbf91a834ace5c2029dee1e24ef5ff2a
MorganA <ma@mark-10.com>
2022-03-03 11:26:23 -0500

    Commit 1 - Example

---
+++ TestProject/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace TestProject.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        public MainWindowViewModel()
+        {
+            ObservableCollection<string> buildUp = new();
+            for (var i = 0; i < 1000; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        buildUp.Add("This is the first item");
+                        break;
+                    case 999:
+                        buildUp.Add("This is the last item");
+                        break;
+                    default:
+                        buildUp.Add("This is item " + i );
+                        break;
+                }
+            }
+
+            Rows = buildUp;
+        }
+
+        public ObservableCollection<string> Rows { get; set; }
+
+
+    }
+}
